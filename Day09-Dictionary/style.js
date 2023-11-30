9946d211dcab57dd33ea94669561c268cb29ac92
Saurav Singh <saurav0896@outlook.com>
2023-11-30 18:22:49 +0530

    Update style.js

--- Day09-Dictionary/style.js
+++ Day09-Dictionary/style.js
@@ -1,16 +1,18 @@
 const generatedText = document.getElementById("textGenerated");
 const generateBtn = document.getElementById("generateBtn");
 const generateTitle = document.getElementById("title");
+const errorText = document.getElementById("error");
 
 // To get the API key register on https://api-ninjas.com/ 
 var API_Key = "API KEY HERE"
 
+
 generateBtn.addEventListener("click", () => {
   const length = document.getElementById("textInput");
-  
+
   fetch(`https://api.api-ninjas.com/v1/dictionary?word=${length.value}`, {
     method: "GET",
-    headers: { "X-Api-Key": API_Key }
+    headers: { "X-Api-Key": "nsMP8qzPg/N7mrxwPewGCA==ZEOoCmgCRHKcG2wU" }
   })
     .then(function (response) {
       if (response.ok) {
@@ -20,11 +22,17 @@ generateBtn.addEventListener("click", () => {
       }
     })
     .then(function (data) {
-      generateTitle.textContent = JSON.parse(data).word + " :";
-      generatedText.textContent = JSON.parse(data).definition;
+      if (JSON.parse(data).valid) {
+        errorText.textContent = "";
+        generateTitle.textContent = JSON.parse(data).word + " :";
+        generatedText.textContent = JSON.parse(data).definition;
+      } else {
+        generateTitle.textContent = "";
+        generatedText.textContent = "";
+        errorText.textContent = "Could not find definition for this word.";
+      }
     })
     .catch(function (error) {
       console.error("Error: ", error);
     });
 });
-
